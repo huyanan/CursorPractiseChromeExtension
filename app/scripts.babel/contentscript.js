5a14da5d6d20c3198a8b7e35a441ed1133a49ece
扈亚楠 <huyn@51cto.com>
2018-08-23 10:34:21 +0800

    shoot

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,67 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Content script');
+var circle = null;
+// 获取随机位置
+function getRandomPos(){
+  const xStart = window.scrollX;
+  const yStart = window.scrollY;
+  const screenW = document.documentElement.clientWidth;
+  const screenH = document.documentElement.clientHeight;
+  let x = 0;
+  let y = 0;
+  x = Math.random()*screenW + xStart;
+  y = Math.random()*screenH + yStart;
+  return {
+    x,
+    y
+  }
+}
+function renderCircle({x,y}) {
+  circle = document.createElement('div');
+  circle.className = 'shoot-me';
+  circle.style.position = 'absolute';
+  circle.style.left = x+'px';
+  circle.style.top = y+'px';
+  circle.addEventListener('click', onClickShootMe);
+  document.body.appendChild(circle);
+}
+
+function onClickShootMe(event) {
+  removeCircle();
+  createCircle();
+}
+
+function createCircle() {
+  renderCircle(getRandomPos());
+}
+function removeCircle() {
+  if (!circle) {
+    return;
+  }
+  circle.removeEventListener('click', onClickShootMe);
+  circle.parentNode.removeChild(circle);
+  circle = null;
+}
+
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        // alert("前端/后端/Popup收到");
+        // console.log(request, sender);
+        freshPage(request.shoot);
+        sendResponse('popup返回值');
+    }
+);
+chrome.storage.sync.get('shoot', function (items) {
+  console.log(items);
+  freshPage(items.shoot)
+});
+// createCircle();
+function freshPage (shoot) {
+  if (shoot) {
+    removeCircle();
+    createCircle();
+  } else {
+    removeCircle();
+  }
+}
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,36 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+var shootBtn = document.getElementById('shoot-button');
+// var checked = window.localStorage.getItem('shoot');
+chrome.storage.sync.get('shoot', function (items) {
+  console.log(items);
+  var shoot = items.shoot;
+  shootBtn.shoot = shoot;
+  shootChange(shoot);
+});
+
+shootBtn.addEventListener('click', shootChange);
+
+function shootChange() {
+  var checked = shootBtn.checked;
+  // window.localStorage.setItem('shoot',checked);
+  chrome.storage.sync.set({
+    'shoot': checked
+  }, function (items) {
+    console.log(items);
+  });
+  sendMessage(checked);
+}
+
+function sendMessage(checked) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){  
+      chrome.tabs.sendMessage(tabs[0].id, {message:'calculate', shoot: checked}, function(response) {
+          if(typeof response !='undefined'){
+              // alert(response);
+          }else{
+              // alert("response为空=>"+response);
+          }
+      });//end  sendMessage   
+  }); //end query
+}
